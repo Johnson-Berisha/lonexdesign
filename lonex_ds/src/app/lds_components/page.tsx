fe6f7388c38963b027721da7fa0ad8cc26cc02aa
Gjonson <gjonsonberisha@gmail.com>
2026-03-25 20:15:06 +0100

    library lookin coool

--- lonex_ds/src/app/lds_components/page.tsx
+++ lonex_ds/src/app/lds_components/page.tsx
@@ -13,9 +13,28 @@ export default function componentsLibrary() {
                         <li className={styles.componentItem}>Forms</li>
                         <li className={styles.componentItem}>Typography</li>
                     </ul>
+                    <span className={styles.subtitle}>MODULES</span>
+                    <ul className={styles.componentList}>
+                        <li className={styles.componentItem}>Buttons</li>
+                        <li className={styles.componentItem}>Cards</li>
+                        <li className={styles.componentItem}>Modals</li>
+                    </ul>
                 </aside>
                 <main className={styles.mainContent}>
-                    <h1>content</h1>
+                    <section className={styles.componentHeader}>
+                        <h1>Button</h1>
+                        <p>It's a button, what else can I say?</p>
+                    </section>
+                    <section className={styles.previewStage}>
+                        <div className="canvas">
+                            <button className="btn">Button</button>
+                        </div>
+                    </section>
+                    <section className={styles.codePreview}>
+                        <div className={styles.codeBlock}>
+                            <code>&lt;button class="lds-btn"&gt;Button&lt;/button&gt;</code>
+                        </div>
+                    </section>
                 </main>
             </div>
         </main>
