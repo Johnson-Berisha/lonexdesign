17a266e2915a462c5cde80a45151e3f1d111eb93
Gjonson <gjonsonberisha@gmail.com>
2026-04-06 10:15:07 +0200

    props actually affect the preview now

--- lonex_ds/src/app/components/test_components/btn.tsx
+++ lonex_ds/src/app/components/test_components/btn.tsx
@@ -1,10 +1,24 @@
 "use client";
 
 import styles from './components.module.css';
+interface BtnProps {
+    variant?: 'primary' | 'secondary' | 'tretiary';
+    disabled?: boolean;
+    rounded?: boolean;
+    label?: string;
+}
 
-const Btn: React.FC = () => {
+const Btn: React.FC<BtnProps> = ({ variant = 'primary', rounded = false, disabled = false, label = 'Button' }) => {
+    let btnClass = variant === 'primary' ? styles.btnPrimary : variant === 'secondary' ? styles.btnSecondary : styles.btnTertiary;
+    if (rounded) {
+        btnClass += ` ${styles.btnRounded}`;
+    }
+
+
     return (
-        <button className={styles.btnPrimary}>Primary Button</button>
+        <button className={btnClass} disabled={disabled}>
+            {label}
+        </button>
     )
 }
 
