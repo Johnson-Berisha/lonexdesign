d9ba187a9ee0aefc37a5565b4a545e4f97ef67df
Gjonson <gjonsonberisha@gmail.com>
2026-04-10 12:30:32 +0200

    made mini alerts. waiting for fetch from pagescms

--- lonex_ds/src/app/components/test_components/alert.tsx
+++ lonex_ds/src/app/components/test_components/alert.tsx
@@ -4,10 +4,18 @@ import styles from './components.module.css';
 
 interface AlertProps {
     variant?: 'danger' | 'info' | 'success' | 'warning';
+    mini?: boolean;
 }
 
-const Alert: React.FC<AlertProps> = ({ variant = 'warning' }) => {
+const Alert: React.FC<AlertProps> = ({ variant = 'warning', mini = false }) => {
     const alertClass = styles.alert + " " + styles[`alert${variant.charAt(0).toUpperCase() + variant.slice(1)}`];
+    if (mini) {
+        return (
+            <div className={alertClass + ' ' + styles.alertMini}>
+                <strong>{variant.charAt(0).toUpperCase() + variant.slice(1)}!</strong>
+            </div>
+        )
+    }
     return (
         <div className={alertClass}>
             <strong>{variant.charAt(0).toUpperCase() + variant.slice(1)}!</strong> This is an alert message.
