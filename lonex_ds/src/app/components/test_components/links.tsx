734a8f9a794dae30937816a371cd81ec93f2cebc
Gjonson <gjonsonberisha@gmail.com>
2026-04-12 21:43:40 +0200

    links props aint working, they require a rename prob

--- lonex_ds/src/app/components/test_components/links.tsx
+++ lonex_ds/src/app/components/test_components/links.tsx
@@ -1,7 +1,13 @@
-import Link from "next/link"
+import Link from "next/link";
+import styles from "./components.module.css";
 
-export default function Links() {
+interface LinksProps {
+    colorHoverState?: boolean;
+}
+
+export default function Links({ colorHoverState }: LinksProps) {
+    const linkClassName = colorHoverState ? styles.linkHover : styles.link;
     return <div>
-        <Link href="#">This is a Link!</Link>
+        <Link className={`${linkClassName}`} href="#">This is a Link!</Link>
     </div>
 }
